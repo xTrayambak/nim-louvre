2f62813e16f22065701ea600655032a7b634f529
Trayambak Rai <xtrayambak@disroot.org>
2025-04-28 16:02:58 +0530

    add: implement `LGPU::fd` and `LGPU::roFd`

--- src/louvre/gpu.nim
+++ src/louvre/gpu.nim
@@ -1,3 +1,10 @@
+## GPU Information
+##
+## This class typically contains information about a DRM node, however, its properties
+## could have different meanings when using virtualized backends.
+##
+## To see all available devices, use `func getGpus(seat: var Seat): seq[ptr GPU]`_. For a particular output, use `func gpu(output: Output): ptr GPU`_.
+
 import std/posix
 import pkg/cppstl/std_string
 
@@ -8,6 +15,8 @@ type GPU* {.importcpp: "Louvre::LGPU".} = object
 func name(gpu: GPU): CppString {.importcpp: "Louvre::LGPU::name".}
 func getDevice*(gpu: GPU): Dev {.importcpp: "Louvre::LGPU::dev".}
 func getId*(gpu: GPU): int {.importcpp: "Louvre::LGPU::id".}
+proc getFd*(gpu: GPU): cint {.importcpp: "Louvre::LGPU::fd".}
+proc getReadOnlyFd*(gpu: GPU): cint {.importcpp: "Louvre::LGPU::roFd".}
 
 {.pop.}
 
